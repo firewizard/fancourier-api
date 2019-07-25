0d665eee1b7d0d17ef482e0509cc788f51260919
Cristian Nicolescu <cristi@mandagreen.com>
2019-07-25 10:53:34 +0300

    fixed autoload path

--- src/autoload.php
+++ src/autoload.php
@@ -1,24 +1,24 @@
 <?php
 
-require_once __DIR__ . '/Auth.php';
-require_once __DIR__ . '/Client.php';
+require_once __DIR__ . '/Fancourier/Auth.php';
+require_once __DIR__ . '/Fancourier/Client.php';
 
-require_once __DIR__ . '/Response/ResponseInterface.php';
-require_once __DIR__ . '/Response/CreateAwb.php';
-require_once __DIR__ . '/Response/DeleteAwb.php';
-require_once __DIR__ . '/Response/Generic.php';
-require_once __DIR__ . '/Response/GetRates.php';
-require_once __DIR__ . '/Response/TrackAwbBulk.php';
+require_once __DIR__ . '/Fancourier/Response/ResponseInterface.php';
+require_once __DIR__ . '/Fancourier/Response/CreateAwb.php';
+require_once __DIR__ . '/Fancourier/Response/DeleteAwb.php';
+require_once __DIR__ . '/Fancourier/Response/Generic.php';
+require_once __DIR__ . '/Fancourier/Response/GetRates.php';
+require_once __DIR__ . '/Fancourier/Response/TrackAwbBulk.php';
 
-require_once __DIR__ . '/Request/AbstractRequest.php';
-require_once __DIR__ . '/Request/CreateAwb.php';
-require_once __DIR__ . '/Request/DeleteAwb.php';
-require_once __DIR__ . '/Request/GetRates.php';
-require_once __DIR__ . '/Request/PrintAwb.php';
-require_once __DIR__ . '/Request/PrintAwbHtml.php';
-require_once __DIR__ . '/Request/RequestCourier.php';
-require_once __DIR__ . '/Request/RequestInterface.php';
-require_once __DIR__ . '/Request/TrackAwb.php';
-require_once __DIR__ . '/Request/TrackAwbBulk.php';
+require_once __DIR__ . '/Fancourier/Request/AbstractRequest.php';
+require_once __DIR__ . '/Fancourier/Request/CreateAwb.php';
+require_once __DIR__ . '/Fancourier/Request/DeleteAwb.php';
+require_once __DIR__ . '/Fancourier/Request/GetRates.php';
+require_once __DIR__ . '/Fancourier/Request/PrintAwb.php';
+require_once __DIR__ . '/Fancourier/Request/PrintAwbHtml.php';
+require_once __DIR__ . '/Fancourier/Request/RequestCourier.php';
+require_once __DIR__ . '/Fancourier/Request/RequestInterface.php';
+require_once __DIR__ . '/Fancourier/Request/TrackAwb.php';
+require_once __DIR__ . '/Fancourier/Request/TrackAwbBulk.php';
 
-require_once __DIR__ . '/Fancourier.php';
+require_once __DIR__ . '/Fancourier/Fancourier.php';
